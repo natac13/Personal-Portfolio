580ff6cf04e614dcc672b3325b7213fe8a53d748
natac13 <sean.campbell13@gmail.com>
2015-09-27 17:55:34 -0400

    changed to compass from just sass got browser-sync running started on the baseline font-sizes

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,34 +4,46 @@ import gulp       from 'gulp';
 import babel      from 'gulp-babel';
 import uglify     from 'gulp-uglify';
 import rename     from 'gulp-rename';
-import sass       from 'gulp-sass';
+//import sass       from 'gulp-sass';
 import del        from 'del';
 import minifyCSS  from 'gulp-minify-css';
-import sourcemaps from 'gulp-sourcemaps';
-import eslint     from 'gulp-eslint';
-import plumber    from 'gulp-plumber';
-import prefixer   from 'gulp-autoprefixer';
+import sourcemaps  from 'gulp-sourcemaps';
+import eslint      from 'gulp-eslint';
+import plumber     from 'gulp-plumber';
+import prefixer    from 'gulp-autoprefixer';
+import cache       from 'gulp-cached';
+import imagemin    from 'gulp-imagemin';
+import remember    from 'gulp-remember';
+import concat      from 'gulp-concat';
+import compass     from 'gulp-compass';
+import browserSync from 'browser-sync';
 
-
+const reload      = browserSync.reload;
 const lintOptions = {
     rulePath: "./",
     useEslintrc: true
 };
 
 const paths = {
-    js: 'js/main.js',
+    js: 'js/*.js',
     basescss: 'css/base.scss',
-    allscss: 'css/*.scss'
+    allscss: 'css/*.scss',
+    imgs: 'images/*',
+    html: './index.html'
 }
+
 
 gulp.task('scripts', function() {
     gulp.src(paths.js)
+        .pipe(cache('js'))
         .pipe(plumber())
         .pipe(eslint(lintOptions))
         .pipe(eslint.format())
         .pipe(eslint.failOnError())
         .pipe(sourcemaps.init())
         .pipe(babel())
+        .pipe(remember('js'))
+        .pipe(concat('main.js', {newLine: ';'}))
         .pipe(uglify())
         .pipe(rename({
             basename: "main",
@@ -39,14 +51,20 @@ gulp.task('scripts', function() {
             extname: ".js"
           }))
         .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('js/dist'));
+        .pipe(gulp.dest('js/dist'))
+        .pipe(reload({stream: true}));
     });
 
 gulp.task('css', function() {
     gulp.src(paths.basescss)
+        .pipe(plumber())
         .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(prefixer())
+        //.pipe(sass().on('error', sass.logError))
+        .pipe(compass({
+            sass: 'css/',
+            css: 'css/dist'
+            }))
+        .pipe(prefixer(['last 2 versions']))
         .pipe(minifyCSS())
         .pipe(rename({
             basename: "style",
@@ -55,12 +73,32 @@ gulp.task('css', function() {
             }))
         .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('css/dist/'))
+        .pipe(reload({stream: true}));
     });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.js, ['scripts']);
-    gulp.watch(paths.allscss, ['css'])
+gulp.task('serve', ['css'], function() {
+
+    browserSync({
+        server: "./"
     });
+
+    gulp.watch(paths.js, ['scripts']);
+    gulp.watch(paths.allscss, ['css']);
+    gulp.watch(paths.html).on('change', reload);
+});
+
+gulp.task('imgmin', () => {
+    gulp.src(paths.imgs)
+        .pipe(imagemin())
+        .pipe(gulp.dest('img/'));
+});
+
+//gulp.task('watch', function() {
+    //gulp.watch(paths.js, ['scripts']);
+    //gulp.watch(paths.allscss, ['css']);
+    //gulp.watch(paths.imgs, ['imgmin']);
+    //});  add to default if uncomment
+
 
 gulp.task('delete', function() {
         del(['js/dist/*', 'css/dist/*'], function(error) {
@@ -68,4 +106,6 @@ gulp.task('delete', function() {
             });
     });
 
-gulp.task('default', ['delete', 'scripts', 'css', 'watch']);
+// had to move the 'css' task before scripts so that when serve runs css has
+// completed and loads the stylesheet.
+gulp.task('default', ['delete', 'css', 'scripts', 'serve']);
