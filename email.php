0172f3ed7934d343d3872d62cbf2e3f197a10bec
natac13 <sean.campbell13@gmail.com>
2015-10-09 12:24:13 -0400

    added shrinkwrap

--- email.php
+++ email.php
@@ -1,24 +1,26 @@
 <?php
 
+function died($error) {
+    echo "There was an error with you submission.<br><br>";
+    echo $error;
+    die();
+}
+
     if(isset($_POST['from-email'])) {
         $email_to = 'sean.campbell13@gamil.com';
         $subject  = $_POST['e-subject'];
         $message  = $_POST['e-message'];
         $email    = $_POST['from-email'];
 
-        function died($error) {
-            echo "There was an error with you submission.<br><br>";
-            echo $error;
-            die();
-        }
+
 
         if(!isset($_POST['e-message'])) {
             died("No email.");
         };
 
         $error_message = "";
-        $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
-        if(!preg_match($email_exp, $email_from)) {
+        $email_exp = '/^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/im';
+        if(!preg_match($email_exp, $email)) {
           $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
         };
         if(strlen($message) < 2) {
@@ -41,6 +43,8 @@
         'X-Mailer: PHP/' . phpversion();
 
         @mail($email_to, $subject, $message, $headers);
+    } else {
+        died("Form not correct");
     }
 ?>
 
